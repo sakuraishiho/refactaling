cbd2640496e681f4ab8a5e063676acd552bc557e
Kaito Fukai <k.fukai@everyleaf.com>
2021-01-16 09:56:13 +0900

    ファクトリを一通り用意する

---
+++ spec/factories/club.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :club do
+    name { 'hoge' }
+    established_on { 1900 }
+    hometown { 'hoge' }
+    country { 'hoge' }
+    color { 'hoge' }
+    manager { 'hoge' }
+  end
+end
